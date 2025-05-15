e76b0469b803844038492064fcb31a95fcf91d8c
David Tobing <saddamtbg@gmail.com>
2025-05-15 23:09:47 +0700

    product-detail header

--- apps/ng-threeparty/src/app/page/product/product-details/product-details.component.ts
+++ apps/ng-threeparty/src/app/page/product/product-details/product-details.component.ts
@@ -3,6 +3,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { CheckDateComponent } from '../check-date/check-date.component';
 import { ProductCommentsComponent } from '../product-comments/product-comments.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { SortProductComponent } from "../sort-product/sort-product.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
@@ -11,7 +14,10 @@ import { ProductCommentsComponent } from '../product-comments/product-comments.c
     MatIconModule,
     CheckDateComponent,
     ProductCommentsComponent,
-  ],
+    MatToolbarModule,
+    SortProductComponent,
+    RouterLink
+],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
